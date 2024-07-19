0b4d1eb17229a87fcfc339109d7bc28f5c49b35e
Vishvaa Darshan <vishvaadarshan26@gmail.com>
2024-07-19 13:18:55 +0530

    Added Transformation and fixed Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,8 +10,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-from src.components.model_trainer import ModelTrainerConfig
-from src.components.model_trainer import ModelTrainer
+
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
