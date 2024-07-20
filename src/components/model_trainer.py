07e5ec487a05f409cfcc43a4134d1fcc7896b861
Vishvaa Darshan <vishvaadarshan26@gmail.com>
2024-07-20 11:12:38 +0530

    New branch modified

---
+++ src/components/model_trainer.py
@@ -0,0 +1,79 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import(
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor   
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object, evaluate_models
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+        
+    def initiate_model_trainer(self,train_array,test_array):
+        try:
+            logging.info("Splitting the data into training and test data")
+            X_train, y_train, X_test, y_test=(
+                train_array[:,:-1], 
+                train_array[:,-1],
+                test_array[:,:-1],  
+                test_array[:,-1]
+            )
+            models = {
+                "Linear Regression": LinearRegression(),
+                "Decision Tree Regressor": DecisionTreeRegressor(),
+                "Random Forest Regressor": RandomForestRegressor(),
+                "KNeighbors Regressor": KNeighborsRegressor(),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+                "Gradient Boosting Regressor": GradientBoostingRegressor(),
+                "XGBoost Regressor": XGBRegressor(),
+                "CatBoost Regressor": CatBoostRegressor(),
+            }   
+            
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models)
+            
+            best_model_score = max(sorted(model_report.values()))
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            
+            best_model = models[best_model_name]
+            
+            if best_model_score<0.6:
+                raise CustomException("No best Model Found")
+            
+            logging.info(f"Best found model on both training and testing dataset")
+            
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj = best_model
+            )
+            
+            predicted=best_model.predict(X_test)
+            
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+            
+            
+        except Exception as e:
+            raise CustomException(e, sys)
+            
+        
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
     try:
@@ -18,3 +19,29 @@ def save_object(file_path, obj):
             
     except Exception as e:
         raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+        
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            
+            model.fit(X_train, y_train) #training the model 
+            
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+            
+            report[list(models.keys())[i]] = test_model_score
+            
+        return report
+    
+    except Exception as e:
+        raise CustomException(e, sys)
+            
+            
+               
+        
