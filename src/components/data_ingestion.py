31ab269b5aa41ba717b17dd6b9114232244060aa
priyan <fasdfasdf>
2024-10-09 23:32:17 +0530

    commit by priyan

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,7 +3,7 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
-
+import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
